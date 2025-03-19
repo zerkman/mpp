d1332b37c3daf2b0f99ae63f63c0b5bc90d85b8d
François Galea <fgalea@free.fr>
2025-03-19 20:08:44 +0100

    Added example code for the mppdec library.

---
+++ example.s
@@ -0,0 +1,130 @@
+;---------------------------------------------------------------------
+;	Example program to display an MPP image from memory using the mppdec library
+;	by Zerkman / Sector One
+;---------------------------------------------------------------------
+
+; Copyright (c) 2012-2025 Francois Galea <fgalea at free.fr>
+; This program is free software. It comes without any warranty, to
+; the extent permitted by applicable law. You can redistribute it
+; and/or modify it under the terms of the Do What The Fuck You Want
+; To Public License, Version 2, as published by Sam Hocevar. See
+; the COPYING file or http://www.wtfpl.net/ for more details.
+
+	opt	p=68000
+
+	section	text
+
+	clr.l	-(sp)
+	move	#32,-(sp)	; Super
+	trap	#1
+	addq.l	#6,sp
+	move.l	d0,-(sp)
+
+	lea	-128(sp),sp
+	move.l	sp,a0
+	movem.l $ffff8240.w,d1-d7/a1
+	movem.l d1-d7/a1,(a0)
+	add	#32,a0
+	move.l	$ffff8200.w,(a0)+
+	move.b	$ffff820a.w,(a0)+
+	move.b	$ffff8260.w,(a0)+
+	move.l	$68.w,(a0)+
+	move.l	$70.w,(a0)+
+	move.l	$114.w,(a0)+
+	move.l	$120.w,(a0)+
+	move.l	$134.w,(a0)+
+	move.b	$fffffa07.w,(a0)+
+	move.b	$fffffa09.w,(a0)+
+	move.b	$fffffa13.w,(a0)+
+	move.b	$fffffa15.w,(a0)+
+	move.b	$fffffa17.w,(a0)+
+	move.b	$fffffa19.w,(a0)+
+	move.b	$fffffa1b.w,(a0)+
+	move.b	$fffffa1d.w,(a0)+
+	move.b	$fffffa1f.w,(a0)+
+	move.b	$fffffa21.w,(a0)+
+	move.b	$fffffa23.w,(a0)+
+	move.b	$fffffa25.w,(a0)+
+
+	move.l	sp,usp
+
+; *** Initialise the MPP struct
+; screen buffers
+	move.l	#screen+256,d0
+	move.b	#$a0,d0
+	move.l	d0,a0
+	lsr	#8,d0
+	move.l	d0,$ffff8200.w
+	lea	imgstr,a1
+	move.l	a0,picp0(a1)
+	add.l	#(230*276+254)&-256,a0
+	move.l	a0,picp1(a1)
+; palette buffers
+	lea	palett,a0
+	move.l	a0,palp0(a1)
+	add.l	#276*48*2,a0
+	move.l	a0,palp1(a1)
+
+; *** Decode the MPP file
+	lea	image(pc),a0
+	bsr	mpp_decode
+
+; *** Display image
+	bsr	mpp_setup_img
+
+; Main loop that just waits for a press on space bar
+main_loop:
+	move.b	$fffffc02.w,d0
+	cmp.b	#$39,d0
+	bne.s	main_loop
+
+exit:
+	move	#$2700,sr
+
+	move.l	usp,sp
+
+	move.l	sp,a0
+	movem.l (a0)+,d1-d7/a1
+	movem.l d1-d7/a1,$ffff8240.w
+	move.l	(a0)+,$ffff8200.w
+	move.b	(a0)+,$ffff820a.w
+	move.b	(a0)+,$ffff8260.w
+	move.l	(a0)+,$68.w
+	move.l	(a0)+,$70.w
+	move.l	(a0)+,$114.w
+	move.l	(a0)+,$120.w
+	move.l	(a0)+,$134.w
+	move.b	(a0)+,$fffffa07.w
+	move.b	(a0)+,$fffffa09.w
+	move.b	(a0)+,$fffffa13.w
+	move.b	(a0)+,$fffffa15.w
+	move.b	(a0)+,$fffffa17.w
+	move.b	(a0)+,$fffffa19.w
+	move.b	(a0)+,$fffffa1b.w
+	move.b	(a0)+,$fffffa1d.w
+	move.b	(a0)+,$fffffa1f.w
+	move.b	(a0)+,$fffffa21.w
+	move.b	(a0)+,$fffffa23.w
+	move.b	(a0)+,$fffffa25.w
+
+	move	#$2300,sr
+
+	lea	128(sp),sp
+
+	move	#32,-(sp)
+	trap	#1
+	addq.l	#6,sp
+
+	clr	-(sp)
+	trap	#1
+
+
+	include	"mppdec.s"
+
+	section	data
+image:	incbin	"image.mpp"
+
+	section	bss
+imgstr:	ds.b	mppstrsize
+screen:	ds.b	2*(230*276+254)
+palett:	ds.b	2*(276*48*2)
